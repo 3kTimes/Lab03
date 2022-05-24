67d0292071f5e5a6d36b2a137db5d0b2777f908c
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-05-24 19:36:27 +0200

    Update lab03.c
    Update, but code is not running yet

--- Lab03_DAC.X/src/lab03.c
+++ Lab03_DAC.X/src/lab03.c
@@ -31,6 +31,8 @@
 #define DAC_SCK_AD2CFG AD2PCFGLbits.PCFG11
 #define DAC_LDAC_AD2CFG AD2PCFGLbits.PCFG13
 
+volatile uint8_t counterHalfSecound = 0;
+
 void dac_initialize()
 {
     SETBIT(DAC_SDI_AD1CFG);
@@ -50,10 +52,10 @@ void dac_initialize()
     CLEARBIT(DAC_SCK_TRIS); 
     CLEARBIT(DAC_LDAC_TRIS);    // set RD8, RB10, RB11, RB13 as output pins
     
-    SETBIT(DAC_CS_PORT); 
+    CLEARBIT(DAC_CS_PORT); // SETBIT(DAC_CS_PORT); 
     CLEARBIT(DAC_SCK_PORT);
     CLEARBIT(DAC_SDI_PORT);
-    CLEARBIT(DAC_LDAC_PORT);    // set default state: CS=1, SCK=0, SDI=undefined, LDAC=1 (Datasheet MCP4822 Page 23)
+    CLEARBIT(DAC_LDAC_PORT);  // SETBIT(DAC_LDAC_PORT);  // set default state: CS=1, SCK=0, SDI=undefined, LDAC=1 (Datasheet MCP4822 Page 23)
 
 }
 
@@ -87,64 +89,65 @@ void timer_initialize()
     T1CONbits.TCS = 1; // Set Clock Source (external = 1)
     T1CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
     T1CONbits.TSYNC = 0; // T1: Set External Clock Input Synchronization -> no sync
-    PR1 = 64; // Load Timer Periods (???)
+    PR1 = 64; // Load Timer Periods 
     TMR1 = 0x00; // Reset Timer Values
     IPC0bits.T1IP = 0x01; // Set Interrupt Priority (actually Level 1)
     IFS0bits.T1IF = 0; // Clear Interrupt Flags
     IEC0bits.T1IE = 1; // Enable Interrupts
     T1CONbits.TON = 1; // Enable the Timers
-    
-    //Timer 2 -- 2 secound
-    
-    T2CONbits.TON = 0; // Disable the Timers
-    T2CONbits.TCKPS = 0b11; // Set Prescaler 256
-    T2CONbits.TCS = 1; // Set Clock Source (internal = 0)
-    T2CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating  
-    PR2 = 256; // Load Timer Periods (???)
-    TMR2 = 0x00; // Reset Timer Values
-    IPC1bits.T2IP = 0x01; // Set Interrupt Priority (actually Level 1)
-    IFS0bits.T2IF = 0; // Clear Interrupt Flags
-    IEC0bits.T2IE = 1; // Enable Interrupts
-    T2CONbits.TON = 1; // Enable the Timers
-    
-    // Timer 3 -- 1 secound
-    
-    T3CONbits.TON = 0; // Disable the Timers
-    T3CONbits.TCKPS = 0b00; // Set Prescaler 1:1
-    T3CONbits.TCS = 1; // Set Clock Source (external = 1)
-    T3CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
-    PR3 = 128; // Load Timer Periods (highets value possible?)
-    TMR3 = 0x00; // Reset Timer Values
-    IPC2bits.T3IP = 0x01; // Set Interrupt Priority (actually Level 1)
-    CLEARBIT(IFS0bits.T3IF); //= 0; // Clear Interrupt Flags
-    IEC0bits.T3IE = 1; // Enable Interrupts --> (important: all)
-    T3CONbits.TON = 1; // Enable the Timers
-    
 }
 
 
 // interrupt service routine
 
 void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
-{ 
-    TOGGLELED(LED1_PORT);
+{   
+    counterHalfSecound ++;
+    
+    if(counterHalfSecound == 1){   //check whether 0.5 s have passed
+            TOGGLELED(LED1_PORT);  
+        }  
+        
+    if(counterHalfSecound == 5){   //check whether 2.5 s have passed
+            TOGGLELED(LED1_PORT);
+        }
+        
+    if(counterHalfSecound == 7){   //check whether 3.5 s have passed
+            TOGGLELED(LED1_PORT);
+            counterHalfSecound = 0;
+        }
     
     IFS0bits.T1IF = 0;  
 }
 
-void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T2Interrupt(void)
-{ 
-    TOGGLELED(LED1_PORT);
+
+void setDAC (uint16_t value){
+  
+    SETBIT(DAC_CS_PORT); // CLEARBIT(DAC_CS_PORT);  // Set CS bit to zero to start conversation
     
-    IFS0bits.T2IF = 0;  
+    uint8_t i =15;
+    uint16_t store;
+        
+    for(i; i>=0; i--){          // Set Binary for Output signal
+            
+        CLEARBIT(DAC_SCK_PORT);
+        
+        DAC_SDI_PORT = value>>i & BV(0);
+            
+        Nop();
+        SETBIT(DAC_SCK_PORT);
+    }
+    
+    CLEARBIT(DAC_CS_PORT); // SETBIT(DAC_CS_PORT);  // Clear CS bit
+    CLEARBIT(DAC_SDI_PORT); // Clear SDI bit
+    Nop();
+    
+    SETBIT(DAC_LDAC_PORT);
+    Nop();
+    Nop();
+    CLEARBIT(DAC_LDAC_PORT);   
 }
 
-void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T3Interrupt(void)
-{ 
-    TOGGLELED(LED1_PORT);
-    
-    IFS0bits.T3IF = 0;  
-}
 
 /*
  * main loop
@@ -152,6 +155,15 @@ void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T3Interrupt(void)
 
 void main_loop()
 {
+    uint16_t oneVoltage = 1000; 
+    oneVoltage = ~BV(15) | ~BV(13) | BV(12);  // settings for DAC.... Bit 15 to 0 (write ti DACA); Bit 14 don't care; Bit 13 to 0 (4.096V); Bit 12 to 1
+    
+    uint16_t twoPointFiveVoltage = 2500;
+    twoPointFiveVoltage = ~BV(15) | ~BV(13) | BV(12);  // settings for DAC.... Bit 15 to 0 (write ti DACA); Bit 14 don't care; Bit 13 to 0 (4.096V); Bit 12 to 1
+    
+    uint16_t threePointFiveVoltage = 3000;
+    threePointFiveVoltage = ~BV(15) | ~BV(13) | BV(12);  // settings for DAC.... Bit 15 to 0 (write ti DACA); Bit 14 don't care; Bit 13 to 0 (4.096V); Bit 12 to 1
+    
     
     // print assignment information
     lcd_printf("Lab03: DAC");
@@ -160,36 +172,24 @@ void main_loop()
     
     CLEARBIT(LED1_TRIS);   //set LED1 as output
     
+    setDAC(oneVoltage); // set initial 1 voltage
+    
     while(TRUE)
     {  // main loop code
         
-        uint16_t oneVolatge = 1000;
-        uint16_t twoPointFiveVolatage = 2500;
-        uint16_t threePointFiveVolateg = 3000;
+        setDAC(oneVoltage); // set initial 1 voltage
+           
+       /* if(counterHalfSecound == 1){   //check whether 0.5 s have passed
+            setDAC(twoPointFiveVoltage); // set Vout to 2.5V  
+        }  
         
-        CLEARBIT(DAC_CS_PORT);  // Set CS bit to zero to start conversation
-    
-        uint8_t i =15;
-        
-        oneVolatge = ~BV(15) | ~BV(13) | BV(12);  // settings for DAC.... Bit 15 to 0 (write ti DACA); Bit 14 don't care; Bit 13 to 0 (4.096V); Bit 12 to 1
-     
-        for(i; i>-1; i--){          // Set Binary for Output signal
-            CLEARBIT(DAC_SCK_PORT);
-        
-            DAC_SDI_PORT |= oneVolatge & BV(i);
-            
-            Nop();
-            SETBIT(DAC_SCK_PORT);
+        if(counterHalfSecound == 5){   //check whether 2.5 s have passed
+            setDAC(threePointFiveVoltage); // set Vout to 3.5V
         }
-    
-        CLEARBIT(DAC_CS_PORT); // Clear CS bit
-        CLEARBIT(DAC_SDI_PORT); // Clear SDI bit
-        Nop();
-    
-        SETBIT(DAC_LDAC_PORT);
-        Nop();
-        Nop();
-        CLEARBIT(DAC_LDAC_PORT);
+        
+        if(counterHalfSecound == 7){   //check whether 3.5 s have passed
+           setDAC(oneVoltage); // set Vout to 1V
+        }*/
     
     }
 }
